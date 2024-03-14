3df43fdac0dfcdff62c024141787a55b622bc5b1
14 <1765877748@qq.com>
2024-03-14 20:01:03 +0800

    update

--- src/run_s2s_kd.py
+++ src/run_s2s_kd.py
@@ -592,8 +592,8 @@ def main():
         sample['features'] = pooled_sentence
         return sample
     print(raw_datasets)
-    raw_datasets['train'] = raw_datasets["train"].select(range(1000))
-    raw_datasets['test'] = raw_datasets["test"].select(range(100))
+    # raw_datasets['train'] = raw_datasets["train"].select(range(1000))
+    # raw_datasets['test'] = raw_datasets["test"].select(range(100))
     with training_args.main_process_first(desc="dataset map pre-processing"):
         raw_datasets = raw_datasets.map(
             preprocess_function,
