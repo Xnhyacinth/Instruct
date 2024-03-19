07a6aa55f2ec1582ff329673d3191c4f99c76845
14 <1765877748@qq.com>
2024-03-19 15:35:58 +0800

    update

--- scripts/eval_tk_instruct.sh
+++ scripts/eval_tk_instruct.sh
@@ -1,15 +1,22 @@
 set -x
-
 # export CUDA_DEVICE_ORDER="PCI_BUS_ID"
 # export TRANSFORMERS_CACHE=/home/yizhongw/.cache/huggingface
 # allenai/tk-instruct-3b-def-pos
-export CUDA_VISIBLE_DEVICES=0,9
-out=output/t5-base_lr1e-4_warm0.05_kd_r32_allenai_kl_fullprompt/checkpoint-5000
+# export CUDA_VISIBLE_DEVICES=3
+num_gpus=${1:-"1"}
+echo "GPU counts: ${num_gpus}"
+gpus=${2:-"8"}
+echo "GPU: ${gpus}"
+model=${3:-"t5-large"}
+batch_size=${4:-"8"}
+model=google/${model}-lm-adapt
+echo "model: ${model}"
+out="output/${model}_eval"
 python src/run_s2s.py \
     --do_predict \
     --predict_with_generate \
     --evaluation_strategy "no" \
-    --model_name_or_path $out \
+    --model_name_or_path $model \
     --max_source_length 1024 \
     --max_target_length 128 \
     --generation_max_length 128 \
@@ -27,4 +34,4 @@ python src/run_s2s.py \
     --overwrite_output_dir \
     --cache_dir ./cache/ \
     --overwrite_cache \
-    --per_device_eval_batch_size 8
+    --per_device_eval_batch_size ${batch_size}
