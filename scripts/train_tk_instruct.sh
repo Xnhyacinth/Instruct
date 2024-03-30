dc403b894ed7e0eef1e844b8fcaeed6fe90d479a
Xnhyacinth-145 <huanxuanliao@gaminl.com>
2024-03-30 17:23:44 +0000

    update

--- scripts/train_tk_instruct.sh
+++ scripts/train_tk_instruct.sh
@@ -38,7 +38,7 @@ custom=${18:-"0"}
 do_sample=${19:-"0"}
 cache="./cache"
 echo epoch: ${epoch}
-name=experiment_pos_pooler${pos}-${model}_lr${lr}_warm${warmup_ratio}
+name=experiment_pos${pos}_pooler-${model}_lr${lr}_warm${warmup_ratio}
 output_dir=output_pos${pos}_pooler/${model}_lr${lr}_warm${warmup_ratio}
 extra_args="--evaluation_strategy no"
 data_dir=data/splits/default
